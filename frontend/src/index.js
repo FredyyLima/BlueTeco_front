213730878a53a8fb563afddc73735561ae36de00
FredyyLima <fredyy_lima@live.com>
2022-02-19 19:20:22 -0300

    .

---
+++ frontend/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { GlobalStyle } from './styles/global'
+import { Main } from './pages/main'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+ReactDOM.render(
+  <React.StrictMode>
+      <GlobalStyle/>
+      <Main />    
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
